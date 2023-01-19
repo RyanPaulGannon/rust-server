98bd9996fa77417470a7dc8be03ca97541f5bdc7
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-01-19 22:55:07 +0000

    Added loop and break

--- 2. Guessing Game/src/main.rs
+++ 2. Guessing Game/src/main.rs
@@ -9,21 +9,26 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
+    loop {
     println!("Please input your guess...");
 
     let mut guess = String::new();
     
     io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        .read_line(&mut guess)
+        .expect("Failed to read line");
     
     let guess: u32 = guess.trim().parse().expect("Please type a number");
 
-    println!("You guessed: {guess}");
-    
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Equal => println!("Correct!"),
-        Ordering::Greater => println!("Too high!"),
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                print!("Correct");
+                break;
+            }
+        }
     }
 }
