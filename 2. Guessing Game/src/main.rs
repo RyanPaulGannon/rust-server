aaa6342eb9addb6cf2dcef0a3d647310038ef40a
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-01-20 22:55:34 +0000

    Switch from expect call to match expression

--- 2. Guessing Game/src/main.rs
+++ 2. Guessing Game/src/main.rs
@@ -10,15 +10,18 @@ fn main() {
     println!("The secret number is: {secret_number}");
 
     loop {
-    println!("Please input your guess...");
+        println!("Please input your guess...");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
     
-    io::stdin()
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
         
@@ -28,7 +31,7 @@ fn main() {
             Ordering::Equal => {
                 print!("Correct");
                 break;
-            }
+            }       
         }
     }
 }
