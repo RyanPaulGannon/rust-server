4914a3e0d03c01b999088deb99721c87b18aa8e2
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-01-22 18:36:45 +0000

    Mutability

--- 3. Common Programming Concepts/3.1 Variables and Mutability/src/main.rs
+++ 3. Common Programming Concepts/3.1 Variables and Mutability/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // Use 'mut' to allow mutability, e.g. let mut x = 5 will allow the value to be changed
+    // Contants aren't allowed to be used with mut
+    let x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
