b63ea1371a5295c5cdef766630e67c8be1001b6f
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-02-12 21:59:12 +0000

    Responds with index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::*;
@@ -19,23 +20,16 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    
-    /*
-    This will provide the response to the request which looks like:
-
-        Request: GET / HTTP/1.1
-        Host: 127.0.0.1:7926
-        Connection: keep-alive
-        Cache-Control: max-age=0
-        sec-ch-ua: "Chromium";v="110", "Not A(Brand";v="24", "Brave";v="110"
-        sec-ch-ua-mobile: ?0
-        sec-ch-ua-platform: "macOS"
-        Upgrade-Insecure-Requests: 1
-        User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36
-    */
-
     stream.read(&mut buffer).unwrap();
-    let response: &str = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
