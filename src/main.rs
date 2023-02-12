75f911270918ce4a5c1a395fd0218365de45cca0
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-02-12 21:45:37 +0000

    Added fn handle_connection + notes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::*;
+
+// TCP: Transport layer protocol which describes how data is transferred from one node to another
+// HTTP: Application level protocol which describes how the data is structured
 
 fn main() {
+    // :ryan when typed on a phone
     let listener = TcpListener::bind("127.0.0.1:7926").unwrap();
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Established")
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    print!("Request: {}", String::from_utf8_lossy(&buffer[..]))
 }
