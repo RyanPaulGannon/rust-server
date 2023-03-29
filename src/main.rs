32ec538f7f3e191d86ed2caf347c9ef04ea6dfd4
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-03-29 22:47:53 +0100

    Testing dynamic routes

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@
 
 #[get("/")]
 fn index() -> &'static str {
+    "Welcome Page"
+}
+
+#[get("/api")]
+fn api() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/api/<name>")]
+fn name(name: &str) -> String {
+    format!("Hello, {}!", name)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, api, name])
 }
