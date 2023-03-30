600bd9ebd611823c318a1727eb53972a0da7901d
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-03-30 15:13:34 +0100

    Add more routes

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn index() -> &'static str {
 }
 
 #[get("/api")]
-fn api() -> &'static str {
-    "Hello, world!"
+fn get_all_users() -> &'static str {
+    "Hello, API!"
 }
 
 #[get("/api/<name>")]
@@ -15,8 +15,23 @@ fn name(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[post("/api/add-user")]
+fn add_user() -> &'static str {
+    "Add User - Todo"
+}
+
+#[put("/api/update-user")]
+fn update_user() -> &'static str {
+    "Update User - Todo"
+}
+
+#[delete("/api/delete-user")]
+fn delete_user() -> &'static str {
+    "Delete User - Todo"
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, api, name])
+        .mount("/", routes![index, get_all_users, name, add_user, update_user, delete_user])
 }
