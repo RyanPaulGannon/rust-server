bce05ec2f357d33bec2f11da2bb67945679b516d
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-05-01 19:44:48 +0100

    Add migrations using diesel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use rocket::serde::json::Json;
 use serde::{Serialize, Deserialize};
 
+use diesel::pg::PgConnection;
+use diesel::Connection;
+
+use dotenv::dotenv;
+use std::env;
+
 #[macro_use] extern crate rocket;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -60,6 +76,7 @@ fn delete_user() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
+    establish_connection();
     rocket::build()
         .mount("/", routes![index, get_all_users, id, add_user, update_user, delete_user])
 }
