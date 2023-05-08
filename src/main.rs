8101e31efb501a316960b0f5775a8daff3b8462f
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-05-08 19:52:52 +0200

    Update deps and add user struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use postgres::{ Client, NoTls};
+use postgres::Error as PostgresError;
+use std::net::{TcpListener, TcpStream };
+use std::io::{Read, Write};
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
