e706cc21ad65d09579ba1bae679fd952f3e2c770
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-05-19 23:11:54 +0100

    Add set_database fn

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,30 @@ struct User {
     email: String
 }
 
-// const DB_URL: &str = env!("DATABASE_URL");
+const DB_URL: &str = env!("DATABASE_URL");
 
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 Not Found\r\n";
 const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 Internal Server Error\r\n";
 
 fn main() {
-    println!("Hello");
+    if let Err(e) = set_database() {
+        println!("Error: {}", e);
+        return;
+    }
+}
+
+fn set_database() -> Result<(), PostgresError> {
+    let mut client = Client::connect(DB_URL, NoTls);
+
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL,
+        )",
+        &[]
+    )?;
+
+    // start_server()
 }
