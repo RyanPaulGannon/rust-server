f576a62bed27bb92fcee8a64e23cf56376d965db
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-06-23 21:28:27 +0100

    Mongo: Testing connect and .env

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,28 @@
-use postgres::{ Client, NoTls};
-use postgres::Error as PostgresError;
-use std::net::{TcpListener, TcpStream };
-use std::io::{Read, Write};
+use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
 use std::env;
+use std::error::Error;
+use tokio;
+use dotenv::dotenv;
 
-#[macro_use]
-extern crate serde_derive;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+   // Load the MongoDB connection string from an environment variable:
+   let client_uri =
+      env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
 
-#[derive(Serialize, Deserialize)]
-struct User {
-    id: Option<i32>,
-    name: String,
-    email: String
-}
+   // A Client is needed to connect to MongoDB:
+   // An extra line of code to work around a DNS issue on Windows:
+   let options =
+      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+         .await?;
+   let client = Client::with_options(options)?;
 
-const DB_URL: &str = env!("DATABASE_URL");
+   // Print the databases in our MongoDB cluster:
+   println!("Databases:");
+   for name in client.list_database_names(None, None).await? {
+      println!("- {}", name);
+   }
 
-const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
-const NOT_FOUND: &str = "HTTP/1.1 404 Not Found\r\n";
-const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 Internal Server Error\r\n";
-
-fn main() {
-    if let Err(e) = set_database() {
-        println!("Error: {}", e);
-        return;
-    }
-}
-
-fn set_database() -> Result<(), PostgresError> {
-    let mut client = Client::connect(DB_URL, NoTls);
-
-    client.execute(
-        "CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            email VARCHAR NOT NULL,
-        )",
-        &[]
-    )?;
-
-    // start_server()
+   Ok(())
 }
