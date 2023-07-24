23b039bd5823fe11e3daf99a91a4b53132289031
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-07-24 19:23:49 +0100

    `Main`: Build a single threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,11 @@
-use tokio;
-use std::env;
-use dotenv::dotenv;
-use std::error::Error;
-use mongodb::bson::doc;
-use chrono::{TimeZone, Utc};
-use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+use std::net::TcpListener;
 
-pub mod mongo;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7926").unwrap();
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().ok();
-   // Load the MongoDB connection string from an environment variable:
-   let client_uri =
-      env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-   // A Client is needed to connect to MongoDB:
-   // An extra line of code to work around a DNS issue on Windows:
-   let options =
-      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
-         .await?;
-   let client = Client::with_options(options)?;
-
-   // Print the databases in our MongoDB cluster:
-   println!("Databases:");
-   for name in client.list_database_names(None, None).await? {
-      println!("- {}", name);
-   }
-
-   let new_doc = doc! {
-    "title": "Parasite",
-    "year": 2020,
-    "plot": "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.",
-    "released": Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
-    };
-
-    println!("{new_doc}");
-
-    let test = client.database("test").collection("new-test");
-
-    let insert_result = test.insert_one(new_doc.clone(), None).await?;
-    println!("New document ID: {}", insert_result.inserted_id);
-
-   Ok(())
+        println!("Connection established!")
+    }
 }
-
